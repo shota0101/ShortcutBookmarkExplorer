73bb7b50298f810ebae3d000cee7e4f99db8d422
shhayash <shhayash@yahoo-corp.jp>
2017-12-10 16:36:51 +0900

    add folder icon

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # ShortcutBookmarkExplorer
 
+## アイコンクレジット
 
+[Freepik](http://www.freepik.com) from https://www.flaticon.com is licensed by CC 3.0 BY
+
+Icons made by [Smashicons](https://www.flaticon.com/authors/smashicons) from www.flaticon.com is licensed by [CC 3.0 BY](http://creativecommons.org/licenses/by/3.0/)
 
--- loadBookmarks.js
+++ loadBookmarks.js
@@ -1,3 +1,5 @@
+var keies = 'asdfghjklqwertyuiopzxcvbnm1234567890ASDFGHJKLQWERTYUIOPZXCVBNM';
+
 function isFolder(node) {
   if(node.children === undefined) {
     return false;
@@ -21,7 +23,7 @@ function getDomain(url) {
 //   request.send();
 // }
 
-function getHtml(node){
+function createHTML(node){
   var html = '';
   for(var i = 0; i < node.length; i++){
     var title = node[i].title;
@@ -35,17 +37,91 @@ function getHtml(node){
     }
     html += '<hr/>';
   }
-  return html;
+
+  var windowIds = [];
+  var keyAssigns = [];
+  var elements = '';
+  var tabsCount = node.length;
+  var titleLimit = '';
+  var tabElements = '';
+  var tabBlockElements = '';
+  var targetTabId = '';
+  var targetWindowId = '';
+
+  if(tabsCount > 50){
+    titleLimit = 8;
+    tabElementsWidth = '190px';
+    tabBlockWidth = '160px';
+  }else if(tabsCount <= 50 && tabsCount > 34){
+    titleLimit = 16;
+    tabElementsWidth = '260px';
+    tabBlockWidth = '230px';
+  }else if(tabsCount <= 34 && tabsCount > 17){
+    titleLimit = 28;
+    tabElementsWidth = '390px';
+    tabBlockWidth = '360px';
+  }else if(tabsCount <= 17 && tabsCount > 0){
+    titleLimit = 58;
+    tabElementsWidth = '790px';
+    tabBlockWidth = '760px';
+  }
+
+  for(var i = 0; i < node.length; i++){
+    if(i >= keies.length){
+      break;
+    }
+    elements += '<div class="tab_element">';
+    elements += '<div class="key">' + keies[i] + '</div>';
+    elements += '<div class="tab_block">';
+    if( isFolder(node[i]) ) {
+      elements += '<img src="/images/folder64.png" width="16" class="favicon" />';
+    }
+    if(node[i].title === '') {
+      elements += '<span class="tab_title">' + node[i].url.substr(0, titleLimit) + '</span>';
+    } else {
+      elements += '<span class="tab_title">' + node[i].title.substr(0, titleLimit) + '</span>';
+    }
+    elements += '</div>';
+    elements += '<div class="clear"></div>';
+    elements += '</div>';
+
+    // keyAssigns[keies[i]] = node[i].id;
+    // windowIds[tabs[i].id] = tabs[i].windowId;
+  }
+
+  document.getElementById('bookmark_list').innerHTML = elements;
+
+  tabElements = document.getElementsByClassName('tab_element');
+  tabBlockElements = document.getElementsByClassName('tab_block');
+
+  for(var i = 0; i < node.length; i++){
+    if(i >= keies.length){
+      break;
+    }
+    tabElements[i].style.width = tabElementsWidth;
+    tabBlockElements[i].style.width = tabBlockWidth;
+  }
+
 }
 
 window.onload = function() {
   chrome.bookmarks.getTree(function(roots){
 
-    var keies = 'asdfghjklqwertyuiopzxcvbnm1234567890ASDFGHJKLQWERTYUIOPZXCVBNM';
     var bookmarks = [];
     var bookmarkBar = roots[0].children[0].children;
-    var html = getHtml(bookmarkBar);
+    
+    createHTML(bookmarkBar);
+    
+    window.addEventListener('keydown', function(e){
+      if(e.shiftKey){
+        targetTabId = keyAssigns[String.fromCharCode(e.keyCode)];
+      }else{
+        targetTabId = keyAssigns[String.fromCharCode((e.keyCode)).toLowerCase()];
+      }
+      targetWindowId = windowIds[targetTabId];
+      chrome.tabs.update(targetTabId, {selected: true});
+      chrome.windows.update(targetWindowId, {focused: true});
+    });
 
-    document.getElementById('bookmark_list').innerHTML = html;
   });
 }
